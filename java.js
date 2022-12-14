59ec36abfb2fb6f015082ec07d328cd79fb7d088
seif <116313275+sahnoune1@users.noreply.github.com>
2022-12-14 21:09:10 +0100

    java.js

--- java.js
+++ java.js
@@ -1,23 +1,16 @@
 function add(a,b){
-    let x=parseInt(a,10);
-    let y=parseInt(b,10)
-    return(x+y);
+    return((a/1)+(b.slice(0,-1)/1));
+            
 
 }
 function mult(a,b){
-    let x=parseInt(a,10);
-    let y=parseInt(b,10)
-    return(x*y);
+    return((a/1)*(b.slice(0,-1)/1));
 }
 function divi(a,b){
-    let x=parseInt(a,10);
-    let y=parseInt(b,10)
-    return(x/y);
+    return((a/1)/(b.slice(0,-1)/1));
 }
 function sub(a,b){
-    let x=parseInt(a,10);
-    let y=parseInt(b,10)
-    return(x-y);
+    return((a/1)-(b.slice(0,-1)/1));
 }
 function operate (){
     let a=prompt("give me first number")
@@ -38,24 +31,70 @@ function operate (){
 }
 
 const button=document.querySelectorAll(".butt");
-const output1=document.querySelector(".output1");
-const output2=document.querySelector(".output2");
-console.log(output1);
-button.forEach(butt=>{
+const display=document.querySelector(".dis");
+const up=document.querySelector(".up");
+let currentValue="";
+console.log(display);
+button.forEach(button=>{
     
-     butt.addEventListener("click",()=>{
+     button.addEventListener("click",(e)=>{
           
-          output1.textContent=butt.textContent;
-          
-})  ;
+          let content=e.target.innerHTML;
+          if (content==="delete"){
+            up.innerHTML="";
+            display.innerHTML="";
+            currentValue="";
+          }
+          if (content!=="equal" && content!=="delete"){
+          currentValue+=content;
+          console.log(currentValue);
+          display.innerHTML=currentValue;
+          }
 
+
+          if(content==="-" || (content==="/") ||(content==="*")||(content==="+")){
+            up.innerHTML=currentValue;
+            display.innerHTML="";
+            currentValue="";
+          
+          }
+
+          let upDisplay=up.innerHTML.charAt(up.innerHTML.length-1);
+          
+          
+          if(content==="equal"&& upDisplay==="+"){
+            
+            display.innerHTML=add(display.innerHTML,up.innerHTML);
+            up.innerHTML="";
+
+          }
+          if(content==="equal"&& upDisplay==="*"){
+            
+            display.innerHTML=mult(display.innerHTML,up.innerHTML);
+            up.innerHTML="";
+
+          }
+          if(content==="equal"&& upDisplay==="/"){
+            
+            display.innerHTML=divi(display.innerHTML,up.innerHTML);
+            up.innerHTML="";
+
+          }
+          if(content==="equal"&& upDisplay==="-"){
+            
+            display.innerHTML=sub(display.innerHTML,up.innerHTML);
+            up.innerHTML="";
+
+          }
+
+
+
+
+
+
+        
+          
 });
-button.forEach(butt=>{
-    
-    butt.removeEventListener("click",()=>{
-         
-         output1.textContent=butt.textContent;
-         
-})  ;
+
 
 });
