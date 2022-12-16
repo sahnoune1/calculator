4e6a7ee417d78859c7d7337e7708d2343473f37c
seif <116313275+sahnoune1@users.noreply.github.com>
2022-12-16 17:49:23 +0100

    java.js

--- java.js
+++ java.js
@@ -1,34 +1,3 @@
-function add(a,b){
-  return((a/1)+(b.slice(0,-1)/1));
-          
-
-}
-function mult(a,b){
-  return((a/1)*(b.slice(0,-1)/1));
-}
-function divi(a,b){
-  return((a/1)/(b.slice(0,-1)/1));
-}
-function sub(a,b){
-  return((a/1)-(b.slice(0,-1)/1));
-}
-function operate (){
-  let a=prompt("give me first number")
-  let b=prompt("give me second number");
-  let c=prompt("give me the operande")
-  if (c==="+"){
-      return(add(a,b))
-  }
-  else if (c==="*"){
-      return(mult(a,b))
-  }
-  else if (c==="/"){
-      return(divi(a,b))
-  }
-  else if (c==="-"){
-      return(sub(a,b))
-  }
-}
 
 const button=document.querySelectorAll(".butt");
 const display=document.querySelector(".dis");
@@ -124,6 +93,7 @@ button.forEach(button=>{
     
         
       display.innerHTML=result;
+     
     };
    });
 });
