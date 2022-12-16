3a042da07fc6553229ab15d10e1344e7c1bd5fd3
seif <116313275+sahnoune1@users.noreply.github.com>
2022-12-16 17:46:16 +0100

    java.js

--- java.js
+++ java.js
@@ -33,6 +33,10 @@ function operate (){
 const button=document.querySelectorAll(".butt");
 const display=document.querySelector(".dis");
 const up=document.querySelector(".up");
+
+
+
+let dotClicked=false;
 let currentValue="";
 console.log(display);
 button.forEach(button=>{
@@ -45,78 +49,83 @@ button.forEach(button=>{
           display.innerHTML="";
           currentValue="";
         }
-        if (content!=="equal" && content!=="delete"){
+        if (content!=="equal" && content!=="delete"&& content!=="."){
         currentValue+=content;
         console.log(currentValue);
         display.innerHTML=currentValue;
+        dotClicked=false;
         }
+        if (content!=="equal" && content!=="delete" && content==="." && dotClicked===false){
+           if (currentValue.includes(".")){
+            return;
+           }
+           else {
+          currentValue+=content;
+          console.log(currentValue);
+          display.innerHTML=currentValue;
+          dotClicked=true;
+           }
+          }
+        
 
       
 
       
 
         if (content==="equal"){
-              let operations=currentValue.split(/\b/);
+              let operations=currentValue.split(/[\s+|\+|\-|\*|\/]/);
           let result=0;
           console.log(operations);
+          let res=0;
+          let stack=[];
+          for (let i=0; i<operations.length;i++){
+
+            if (operations[i]==="*"){
+              stack=operations.splice(i-1,3);
+              console.log(operations);
+              console.log(stack);
+              res = parseFloat(stack[0])*parseFloat(stack[2]);
+              console.log(res);
+              operations.splice(i-1,0,res);
+              console.log(operations);
+
+              i--;
+            }
+            if (operations[i] === "/") {
+              stack = operations.splice(i - 1, 3);
+              console.log(operations);
+              console.log(stack);
+              res = parseFloat(stack[0]) / parseFloat(stack[2]);
+              console.log(res);
+              operations.splice(i - 1, 0, res);
+              console.log(operations);
+
+              i--;
+            } 
+
+          }
           
-
-          
-              
-
-let res=0;
-let stack=[];
-for (let i=0; i<operations.length;i++){
-
-if (operations[i]==="*"){
-   stack=operations.splice(i-1,3);
-   console.log(operations);
-   console.log(stack);
-res = parseFloat(stack[0])*parseFloat(stack[2]);
-console.log(res);
-operations.splice(i-1,0,res);
-console.log(operations);
-
-i--;
-}
-if (operations[i] === "/") {
-  stack = operations.splice(i - 1, 3);
-  console.log(operations);
-  console.log(stack);
-  res = parseFloat(stack[0]) / parseFloat(stack[2]);
-  console.log(res);
-  operations.splice(i - 1, 0, res);
-  console.log(operations);
-
-  i--;
-} 
-
-}
-          
-            for (let i=0;i<operations.length;i++){
+          for (let i=0;i<operations.length;i++){
          
-             if (operations[i]==="+"){
+            if (operations[i]==="+"){
               result =result+parseFloat(operations[i+1]);
               i=i+1;            
-          }
+            }
       
-        else if (operations[i]==="-"){
-          result =result-parseFloat(operations[i+1]);
-           i=i+1;
-      }
-          else {
-            result+=parseFloat(operations[i]);
+            else if (operations[i]==="-"){
+              result =result-parseFloat(operations[i+1]);
+              i=i+1;
+            }
+            else {
+              result+=parseFloat(operations[i]);
             
-            
-         
-          
-          }
-      }
+            }
+         }
     
         
-display.innerHTML=result;
-};
-});
+      display.innerHTML=result;
+    };
+   });
 });
 
 // The final result will be the top element on the stack
